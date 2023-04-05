5e940b65f76933dcdf3e78298903e4430dc0d8e5
Przemyslaw Gajczewski <przemek0000przemek@gmail.com>
2023-04-05 21:33:44 +0200

    splitted nav for nav and section in app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import { StyledLi, StyledNavLink, StyledUl, StyleApp } from "./styled";
 
 export default function App() {
     return (
-        <nav>
-            <StyledUl>
-                <StyledLi>
-                    <StyledNavLink to={`tasks`}>Zadania</StyledNavLink>
-                </StyledLi>
-                <StyledLi>
-                    <StyledNavLink to={`author`}>O autorze</StyledNavLink>
-                </StyledLi>
-                <StyledLi>
-                    <StyledNavLink to={``}>Demo</StyledNavLink>
-                </StyledLi>
-            </StyledUl>
-            <StyleApp >
-                <Outlet />
-            </StyleApp>
-        </nav>
-
+        <>
+            <nav>
+                <StyledUl>
+                    <StyledLi>
+                        <StyledNavLink to={`tasks`}>Zadania</StyledNavLink>
+                    </StyledLi>
+                    <StyledLi>
+                        <StyledNavLink to={`author`}>O autorze</StyledNavLink>
+                    </StyledLi>
+                    <StyledLi>
+                        <StyledNavLink to={``}>Demo</StyledNavLink>
+                    </StyledLi>
+                </StyledUl>
+            </nav>
+            <section>
+                <StyleApp >
+                    <Outlet />
+                </StyleApp>
+            </section>
+        </>
     )
 }
 
