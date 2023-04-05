a0e37fdb57bd733b7c20d2b19709aa651e5f07d4
Przemyslaw Gajczewski <przemek0000przemek@gmail.com>
2023-04-05 21:38:26 +0200

    formated text in author page

--- src/pages/authorPage/index.js
+++ src/pages/authorPage/index.js
@@ -10,9 +10,9 @@ const Author = () => {
             <Section
                 title="O mnie"
                 body={<div>Od zawsze interesowałem się komputerami 💻, swój pierwszy komputer dostałem w wieku 8 lat. Obecnie ucze się Frontend Development z YouCode
-                    do tworzenia aplikacji internetowych. W wolnych
-                    chwilach uprawiam sport - bieganie 🏃‍♂️, siłownia 💪. Interesuje mnie również rynek gdzie regularnie inwestuje na giełdzie. Z muzyki to lubię
-                    grać na pianinie 🎹, a jeśli chodzi o słuchanie to zależy od sytuacji w jakiej się znajduje lub nastroju - rock, dark techno, ambient, trance, muzyka filmowa, soundtracks.
+                    do tworzenia aplikacji internetowych. W wolnych chwilach uprawiam sport - bieganie 🏃‍♂️, siłownia 💪. Interesuje mnie również rynek gdzie regularnie 
+                    inwestuje na giełdzie. Z muzyki to lubię grać na pianinie 🎹, a jeśli chodzi o słuchanie to zależy od sytuacji w jakiej się znajduje lub nastroju 
+                    - rock, dark techno, ambient, trance, muzyka filmowa, soundtracks.
                 </div>}
             />
         </ StyledDiv>
